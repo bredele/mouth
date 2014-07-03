18b6cb92e5253aacc9b02d059cbd2be0048917fa
bredele <olivier.wietrich@gmail.com>
2014-07-02 21:18:14 -0600

    add basic tests

--- test/mouth.js
+++ test/mouth.js
@@ -5,3 +5,26 @@
 
 var mouth = require('..');
 var assert = require('assert');
+
+describe("substitution", function() {
+	
+	var tmpl;
+	beforeEach(function() {
+		tmpl = mouth({
+			beep: 'boop',
+			foo: 'bar'
+		});
+	});
+
+	it("should substitue a single variable", function() {
+		var result = tmpl('${ beep }');
+		assert.equal(result, 'boop');
+	});
+
+	it("should substitue multiple variables", function() {
+		var result = tmpl('${ beep } and ${ foo }');
+		assert.equal(result, 'boop and bar');
+	});
+
+	
+});

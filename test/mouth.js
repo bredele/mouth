795eaf45c60a5d718726544f9c969e5a13b3a930
bredele <olivier.wietrich@gmail.com>
2014-10-20 20:38:03 -0600

    start refactoring new api

--- test/mouth.js
+++ test/mouth.js
@@ -6,51 +6,80 @@
 var mouth = require('..');
 var assert = require('assert');
 
-describe("substitution", function() {
-	
-	var tmpl;
-	beforeEach(function() {
-		tmpl = mouth({
-			beep: 'boop',
-			foo: 'bar'
-		});
-	});
+describe('substitution', function() {
 
 	it("should substitue a single variable", function() {
-		var result = tmpl('${ beep }');
-		assert.equal(result, 'boop');
+		var tmpl = mouth('hello ${name}');
+		var result = tmpl({
+			name: 'boop'
+		});
+
+		assert.equal(result, 'hello boop');
 	});
 
 	it("should substitue multiple variables", function() {
-		var result = tmpl('${ beep } and ${ foo }');
+		var tmpl = mouth('${ beep } and ${ foo }');
+		var result = tmpl({
+			beep: 'boop',
+			foo: 'bar'
+		});
+
 		assert.equal(result, 'boop and bar');
 	});
 
 	it('should be dead simple', function() {
-		assert.equal(mouth({
+		assert.equal(mouth('hello ${hello}!', {
 			hello: 'world'
-		}, 'hello ${hello}!'), 'hello world!');
-	});
-});
-
-describe("plugins", function() {
-
-	var tmpl;
-	beforeEach(function() {
-		tmpl = mouth({
-			link: 'home'
-		});
-	});
-
-	it("should add middlware", function() {
-		tmpl.use('path', function(str) {
-			return '/path/' + this[str.trim()];
-		});
-		var result = tmpl('$path{link}');
-		assert.equal(result, '/path/home');
+		}), 'hello world!');
 	});
 
 });
+
+// describe("substitution", function() {
+	
+// 	var tmpl;
+// 	beforeEach(function() {
+// 		tmpl = mouth({
+// 			beep: 'boop',
+// 			foo: 'bar'
+// 		});
+// 	});
+
+// 	it("should substitue a single variable", function() {
+// 		var result = tmpl('${ beep }');
+// 		assert.equal(result, 'boop');
+// 	});
+
+// 	it("should substitue multiple variables", function() {
+// 		var result = tmpl('${ beep } and ${ foo }');
+// 		assert.equal(result, 'boop and bar');
+// 	});
+
+// 	it('should be dead simple', function() {
+// 		assert.equal(mouth({
+// 			hello: 'world'
+// 		}, 'hello ${hello}!'), 'hello world!');
+// 	});
+// });
+
+// describe("mixins", function() {
+
+// 	var tmpl;
+// 	beforeEach(function() {
+// 		tmpl = mouth({
+// 			link: 'home'
+// 		});
+// 	});
+
+// 	it("should add middlware", function() {
+// 		tmpl.use('path', function(str) {
+// 			return '/path/' + this[str.trim()];
+// 		});
+// 		var result = tmpl('$path{link}');
+// 		assert.equal(result, '/path/home');
+// 	});
+
+// });
 
 describe("filters", function() {
 	

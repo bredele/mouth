610e431b781bceb92100ff4c792cf2206027e3d6
bredele <olivier.wietrich@gmail.com>
2014-10-20 20:50:58 -0600

    add documentation

--- index.js
+++ index.js
@@ -5,6 +5,20 @@
 
 module.exports = function(text, data) {
 
+  /**
+   * Substitute data variables in
+   * template string.
+   *
+   * Examples:
+   *
+   *   var tmpl = mouth('${ beep }');
+   *   tmpl(data);
+   *   
+   * @param  {String} str
+   * @return {String}
+   * @api public
+   */
+  
   var tmpl = function(store) {
     store = store || data;
     return text.replace(/\$\{([^{}]*)\}/g, function(_, expr) {
@@ -14,8 +28,14 @@ module.exports = function(text, data) {
 
   if(data) return tmpl();
 
-  data = data || {};
+
+  /**
+   * Data.
+   * @type {Object}
+   */
   
+  data = data || {};
+
   return tmpl;
 };
 

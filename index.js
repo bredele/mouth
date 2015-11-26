e1e77dee0251bc71c615f2b5d92e74e86c106437
bredele <olivier.wietrich@gmail.com>
2015-11-25 17:26:55 -0700

    basic interpolation

--- index.js
+++ index.js
@@ -4,71 +4,24 @@
  * @param  {RegExp}
  */
 
-var identifier = new RegExp(/\.\w+|"[^"]*"|'[^']*'|\/([^/]+)\/|[a-zA-Z_]\w*/g);
 var brackets = new RegExp(/\$\{([^{}]*)\}/g);
 
 
 /**
- * Compile expression.
  *
- * Examples:
- ę
- *   // compiled expression
- *   var tmpl = mouth('${ beep }');
- *   tmpl.text(data);
  *
- *   // expresssion unique identifiers
- *   tmp.props;
- *   
- * @param  {String} str
- * @return {Object} 
- * @api public
  */
 
 
 module.exports = function(str) {
-  var props = [];
-  var text = str.replace(brackets, function(_, expr) {
-    return '"+' + compile(expr, props) + '+"';
+  var list = [];
+  str = str.replace(brackets, function(_, expr) {
+    list.push(expr);
+    return 'model.' + expr;
   });
-  return {
-    text: Function('o', 'return "' + text + '"'),
-    props: props
-  };
+  return [new Function('model', 'return ' + str), list];
 };
 
 
-/**
- * Separator characters.
- * @type {Array}
- */
-
-var forbidden = ['"', '.'];
-
-
-/**
- * Replace expression
- * identifier.
- *
- * Examples:
- *
- *   compile('name + last');
- *   // => o.name + o.last
- *
- *   compile('name[0]');
- *   // => o.name[0]
- *   
- * @param  {String} str
- * @return {String}
- * @api private
- */
-
-function compile(str, arr) {
-  return str.replace(identifier, function(expr) {
-    if(forbidden.indexOf(expr[0]) > -1) return expr;
-    if(arr.indexOf(expr) < 0) arr.push(expr);
-    return 'o.' + expr;
-  });
-}
 
 
--- test/mouth.js
+++ test/mouth.js
@@ -12,18 +12,18 @@ describe('basic', function() {
 	var interpolate =  mouth('${name}');
 
 	it('should return a function', function() {
-		assert.equal(typeof interpolate[0]);
+		assert.equal(typeof interpolate[0], 'function');
 	});
 
 
 	it('should return a list of variable to interpolate', function() {
-		assert.deepEqual(mouth[1], ['name']);
+		assert.deepEqual(interpolate[1], ['name']);
 	});
 
 	it('should interpolate variable', function() {
 		assert.equal(interpolate[0]({
 			name: 'olivier'
-		}), 'olivier';)
+		}), 'olivier');
 	});
 
 });
